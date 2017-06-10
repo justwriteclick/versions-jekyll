c50650dab248d53ac589942c12ada58725eaa0d1
Anne Gentle <annegentle@justwriteclick.com>
2017-06-09 21:35:24 -0500

    Publishing master to GitHub Pages on 06-09-2017

---
+++ script/build-output.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+bundle exec jekyll build --config _config.yml
+bundle exec jekyll build --config _4.1.yml
+bundle exec jekyll build --config _4.1.1.yml
