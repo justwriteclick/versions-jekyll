8a13a36ca3ef774c3101de9541080ba0c1919079
Anne Gentle <annegentle@justwriteclick.com>
2017-10-01 19:31:35 -0500

    Fix typo in script for _config.4.1.1.yml

--- script/build-output.sh
+++ script/build-output.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bundle exec jekyll build --config _config.yml,_config4.1.1.yml --destination /4.1.1
+bundle exec jekyll build --config _config.yml,_config.4.1.1.yml --destination /4.1.1
