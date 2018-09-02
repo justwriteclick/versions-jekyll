9d87d192d1b2b41ed74f317e1c97304fdb7f20ba
Anne Gentle <annegentle@justwriteclick.com>
2018-09-02 13:49:53 -0500

    Update build-travis.sh

--- script/build-travis.sh
+++ script/build-travis.sh
@@ -3,4 +3,4 @@ set -e # halt script on error
 
 # Could you build a config file on the fly each time with a base url matching
 # the name of the branch?
-bundle exec jekyll build --config _config.yml
+bundle exec jekyll build --config _config.yml _config.4.2.yml
